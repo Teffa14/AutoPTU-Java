1f9849bd8b8bb333d6893e9231909e397ee0c76f
Teffa14 <98290252+Teffa14@users.noreply.github.com>
2026-08-19 09:55:50 +1000

    Let damage hooks emit semantic events

--- src/main/java/io/autoptu/core/hook/DamageModifierHook.java
+++ src/main/java/io/autoptu/core/hook/DamageModifierHook.java
@@ -1,11 +1,7 @@
 package io.autoptu.core.hook;
 
-import io.autoptu.core.model.AttackModifier;
-
-import java.util.List;
-
 /** One authoritative rule contribution to the damage modifier pipeline. */
 @FunctionalInterface
 public interface DamageModifierHook {
-    List<AttackModifier> resolve(DamageModifierHookContext context);
+    DamageModifierHookResult resolve(DamageModifierHookContext context);
 }
