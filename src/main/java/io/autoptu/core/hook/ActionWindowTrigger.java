0f9f23eec9365cbde66a101478f57a9586bcd97c
Teffa14 <98290252+Teffa14@users.noreply.github.com>
2026-09-14 13:14:46 +1000

    Model all Attack of Opportunity trigger families

--- src/main/java/io/autoptu/core/hook/ActionWindowTrigger.java
+++ src/main/java/io/autoptu/core/hook/ActionWindowTrigger.java
@@ -3,5 +3,9 @@ package io.autoptu.core.hook;
 /** Semantic event that opened an action window. */
 public enum ActionWindowTrigger {
     UNSPECIFIED,
-    ADJACENT_FOE_SHIFTS_AWAY
+    ADJACENT_FOE_SHIFTS_AWAY,
+    ADJACENT_FOE_USES_NON_TARGETING_MANEUVER,
+    ADJACENT_FOE_STANDS_UP,
+    ADJACENT_FOE_USES_RANGED_ATTACK_WITHOUT_ADJACENT_TARGET,
+    ADJACENT_FOE_RETRIEVES_ITEM
 }
